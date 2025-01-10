1c6fd8272a06c2044cafbf89ab9d6a201d88ebd0
THEGAMER221148 <weststefany@icloud.com>
2025-01-10 13:33:14 -0800

    added more to main menu

--- InGame.js
+++ InGame.js
@@ -9,7 +9,7 @@ let mapHeight = 20;
 let blockSize = 10;
 let plr = {x: (mapWidth*blockSize)*2048+(mapWidth/2)*blockSize-5, y: (mapHeight*blockSize)*2048+(mapHeight/2)*blockSize+5, d: 0, vx: 0, vy: 0, radius: 1, moveSpeed: 0, moveDirction: 0};
 let FOV = Math.PI/2;
-let QUALITY = 320;
+let QUALITY = document.getElementById("renderQualitySlider").value;
 let keysDown = {};
 let returnQuality = 0.1;
 const WALLHEIGHT = 4000;
@@ -76,9 +76,10 @@ let loadedEntities = [];
 window.addEventListener("keydown", function(input){keysDown[input.key.toLowerCase()] = true;});
 window.addEventListener("keyup", function(input){keysDown[input.key.toLowerCase()] = false;});
 function start(){
+    QUALITY = document.getElementById("renderQualitySlider").value;
     canvas.style.visibility = 'visible';
     console.log(canvas.style.visibility)
-    document.getElementById("MainMenu").style.visibility = 'hidden';
+    document.getElementById("mainMenu").style.visibility = 'hidden';
     canvas.requestPointerLock();
     if(!started){
         started = true;
@@ -110,7 +111,7 @@ function getTileAt(x, y){
     return map[Math.floor((x%(mapWidth*blockSize))/blockSize)+(Math.floor((y%(mapHeight*blockSize))/blockSize)*mapHeight)];
 }
 
-function checkCollision(x, y, size, q){
+function checkCollision(x, y, size){
     let collided = false;
     return getTileAt(x, y) != 0 || getTileAt(x+size, y+size) != 0 || getTileAt(x-size, y+size) != 0 || getTileAt(x-size, y-size) != 0 || getTileAt(x-size, y-size) != 0 || collided
 }
